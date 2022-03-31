ba14ce0519a76bbda5f428e3b995771f6c0e9deb
malikoc2020 <malikoc2020@gmail.com>
2022-04-01 00:54:00 +0300

    Business Katmanı Projeye Eklendi.

---
+++ Homework05-Business/Abstracts/IUserService.cs
@@ -0,0 +1,24 @@
+﻿using Homework05_Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework05_Business.Abstracts
+{
+    public interface IUserService
+    {
+        User GetUser(int id);
+        IQueryable<User> GetAllUserAsQueryable();
+
+        void AddUser(User user);
+        void AddUsers(List<User> users);
+
+        void UpdateUser(User user);
+        void UpdateUsers(List<User> users);
+
+        void DeleteUser(User user);
+        void DeleteUsers(List<User> users);
+    }
+}
---
+++ Homework05-Business/Concretes/UserService.cs
@@ -0,0 +1,78 @@
+﻿using Homework05_Business.Abstracts;
+using Homework05_DataAccess.Entityframework.Repository.Abstracts;
+using Homework05_Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework05_Business.Concretes
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public UserService(IRepository<User> repository, IUnitOfWork unitOfWork)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public User GetUser(int id)
+        {
+            return repository.Get(id);
+        }
+
+        public IQueryable<User> GetAllUserAsQueryable()
+        {
+            return repository.GetAll();
+        }
+
+
+        public void AddUser(User user)
+        {
+            repository.Add(user);
+            unitOfWork.Commit();
+        }
+        public void AddUsers(List<User> users)
+        {
+            foreach (var user in users)
+            {
+                repository.Add(user);
+            }           
+            unitOfWork.Commit();
+        }
+
+        public void UpdateUser(User user)
+        {
+            repository.Update(user);
+            unitOfWork.Commit();
+        }
+
+        public void UpdateUsers(List<User> users)
+        {
+            foreach (var user in users)
+            {
+                repository.Update(user);
+            }
+            unitOfWork.Commit();
+        }
+
+        public void DeleteUser(User user)
+        {
+            repository.Delete(user);
+            unitOfWork.Commit();
+        }
+
+        public void DeleteUsers(List<User> users)
+        {
+            foreach (var user in users)
+            {
+                repository.Delete(user);
+            }
+            unitOfWork.Commit();
+        }
+    }
+}
