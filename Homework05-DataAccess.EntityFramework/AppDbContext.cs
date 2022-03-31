5e0f1985e9bb00044a97311b33bb51ce0428ac74
malikoc2020 <malikoc2020@gmail.com>
2022-04-01 00:13:44 +0300

    DataAccess.Entitiyframeworkcore katmanı eklendi.

---
+++ Homework05-DataAccess.EntityFramework/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Homework05_DataAccess.EntityFramework.Configurations;
+using Homework05_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework05_DataAccess.EntityFramework
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
---
+++ Homework05-DataAccess.EntityFramework/Configurations/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Homework05_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework05_DataAccess.EntityFramework.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(x => x.Id);
+
+            //Veritabanı oluşturulurken test için yada başka amaçlarla otomatik veri oluşturulmasını istiyorsak aşağıdaki kod kullanılabilir. SeetUsers() List<User> dönen bir metod olmalı!
+            //builder.HasData(SeedUsers());
+        }
+    }
+}
