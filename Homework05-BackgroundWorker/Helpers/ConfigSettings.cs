d2cf050e1f960156f04e11acd59030bd8179f4ac
malikoc2020 <malikoc2020@gmail.com>
2022-04-01 02:15:36 +0300

    Dependency injection dışında herşey tamam. Bu kapsamda Ara Commit İşlemi.

---
+++ Homework05-BackgroundWorker/Helpers/ConfigSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework05_BackgroundWorker.Helpers
+{
+    public static class ConfigSettings
+    {
+        public static string dbSQLEXPRESS { get; }
+        static ConfigSettings()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+            dbSQLEXPRESS = configurationBuilder.Build().GetSection("ConnectionStrings:DBSQLEXPRESS").Value;
+        }
+    }
+}
--- Homework05-BackgroundWorker/Program.cs
+++ Homework05-BackgroundWorker/Program.cs
@@ -1,3 +1,11 @@
+using Homework05_BackgroundWorker.Helpers;
+using Homework05_Business.Abstracts;
+using Homework05_Business.Concretes;
+using Homework05_DataAccess.Entityframework.Repository.Abstracts;
+using Homework05_DataAccess.Entityframework.Repository.Concretes;
+using Homework05_DataAccess.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -9,6 +17,7 @@ namespace Homework05_BackgroundWorker
 {
     public class Program
     {
+        //public IConfiguration Configuration { get; }
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -18,6 +27,16 @@ namespace Homework05_BackgroundWorker
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
+
+                    //services.AddDbContext<AppDbContext>(ServiceLifetime.Transient);
+
+
+                    /*services.AddDbContext<AppDbContext>(options => options.UseSqlServer(ConfigSettings.dbSQLEXPRESS));
+
+                    services.AddTransient<IUnitOfWork, UnitOfWork>();
+                    services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+                    services.AddTransient<IUserService, UserService>();*/
+
                     services.AddHostedService<Worker>();
                 });
     }
